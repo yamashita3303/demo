a6e8cf80e56296e4152c606d10e260844f1a6250
yamashita <fko2347040@stu.o-hara.ac.jp>
2024-03-13 14:55:31 +0900

    残り全部

---
+++ src/test/java/jp/ac/ohara/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.ohara.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
